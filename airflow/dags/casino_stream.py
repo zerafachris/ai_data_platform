45f7a07456fc2d065070ea0c9044d8cea2c54c75
zerafachris PERSONAL <zerafachris@gmail.com>
2024-02-04 00:11:13 +0100

    casino_stream, clickhouse

---
+++ airflow/dags/casino_stream.py
@@ -0,0 +1,92 @@
+import os
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+from kafka import KafkaProducer
+import time
+import logging
+import json
+import numpy as np
+import random
+
+
+
+
+default_args = {
+    'owner': 'ai_data_platform',
+    'start_date': datetime(2023, 9, 3, 10, 00)
+}
+
+map_game_type = {
+    0 : 'slots',
+    1 : 'live_casino',
+    2 : 'poker',
+    3 : 'bingo',
+    4 : 'roulette',
+    5 : 'black_jack'
+}
+
+
+run_duration = 60*3
+
+def get_casino_data():
+    dt = datetime.now() - timedelta(days=random.randint(0,15))
+    is_bonus = random.uniform(0,1) < 0.2
+    is_win = random.uniform(0,1) < 0.15
+    
+    
+    customer_id = random.randint(0,10000)
+    activity_datetime  = dt.strftime('%Y-%m-%d %H:%M:%S')
+    game_activity = dt.strftime('%Y%m%d%H%M%S%f')
+    game_id = random.randint(0,1000)
+    game_type = map_game_type[ game_id%6]
+    bet_amount = np.round( np.abs(np.random.laplace(0, 20, 1)[0]), 4)
+    win_amount = np.round(bet_amount * np.abs(np.random.laplace(0, 20,1)[0]), 4) if is_win else 0
+    bonus_cost_amount = win_amount if is_bonus and is_win else 0
+
+    
+    
+    casino_data = {
+        'customer_id' : customer_id,
+        'activity_datetime' : activity_datetime,
+        'game_activity' : game_activity,
+        'game_id' : game_id,
+        'game_type': game_type,
+        'bet_amount' : bet_amount,
+        'win_amount' : win_amount,
+        'bonus_amount' : bonus_cost_amount
+    }
+    
+    return casino_data
+
+    
+
+
+def stream_data():
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092' if os.getenv("DEBUG", 'False') == 'True' else 'broker:29092'],
+                             max_block_ms=5000)
+    curr_time = time.time()
+
+    while True:
+        if time.time() > curr_time + run_duration:
+            break
+        res = get_casino_data()
+            
+        logging.info(f'Sending to kafka {res}')
+        producer.send('game_v1', json.dumps(res).encode('utf-8'))
+        time.sleep(0.1)
+
+if os.getenv("DEBUG", 'False') == 'True':
+    stream_data()
+else:
+    logging.info('Starting processing of DAG')
+    with DAG('mock_data',
+            default_args=default_args,
+            schedule_interval='*/5 * * * *',
+            catchup=False) as dag:
+
+        streaming_task = PythonOperator(
+            task_id='generate_casino_data',
+            python_callable=stream_data
+        )
---
+++ clickhouse/docker-entrypoint-initdb.d/init-db.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+set -e
+
+clickhouse client -n <<-EOSQL
+
+    CREATE DATABASE IF NOT EXISTS stage;
+    CREATE DATABASE IF NOT EXISTS dwh;
+    CREATE DATABASE IF NOT EXISTS marts;
+
+    drop table if exists stage.kafka_fact_game;
+    drop table if exists stage.null_fact_game;
+    drop table if exists stage.mv_null_fact_game;
+
+    drop table if exists dwh._fact_game;
+    drop table if exists dwh.mv_fact_game;
+
+    drop table if exists marts._daily_fact_game;
+    drop table if exists marts.mv_daily_fact_game;
+    drop table if exists marts.daily_fact_game;
+
+    -- stage
+    CREATE TABLE IF NOT EXISTS stage.kafka_fact_game
+    (
+        customer_id       Int32,
+        activity_datetime String,
+        game_activity     String,
+        game_id           Int32,
+        game_type LowCardinality(String),
+        bet_amount        Decimal(18, 4),
+        win_amount        Decimal(18, 4),
+        bonus_amount      Decimal(18, 4)
+    )
+        ENGINE = Kafka SETTINGS kafka_broker_list = 'broker:29092', kafka_topic_list = 'game_v1', kafka_group_name = 'ch_game_v1', kafka_format = 'JSONEachRow', kafka_row_delimiter = '\n', kafka_num_consumers = 1, kafka_skip_broken_messages = 1;
+
+
+    CREATE TABLE IF NOT EXISTS stage.null_fact_game
+    (
+        customer_id       Int32,
+        activity_datetime String,
+        game_activity     String,
+        game_id           Int32,
+        game_type LowCardinality(String),
+        bet_amount        Decimal(18, 4),
+        win_amount        Decimal(18, 4),
+        bonus_amount      Decimal(18, 4)
+    ) ENGINE = Null();
+
+
+    CREATE MATERIALIZED VIEW IF NOT EXISTS stage.mv_null_fact_game TO stage.null_fact_game AS
+    SELECT customer_id, activity_datetime, game_activity, game_id, game_type, bet_amount, win_amount, bonus_amount
+        FROM stage.kafka_fact_game;
+
+    -- dwh
+    CREATE TABLE IF NOT EXISTS dwh._fact_game
+    (
+        customer_id       Int32,
+        activity_datetime DateTime,
+        game_activity     String,
+        game_id           Int32,
+        game_type LowCardinality(String),
+        bet_amount        Decimal(18, 4),
+        win_amount        Decimal(18, 4),
+        bonus_amount      Decimal(18, 4)
+    ) ENGINE = MergeTree() PARTITION BY toYYYYMM(activity_datetime) ORDER BY (toYYYYMMDD(activity_datetime), customer_id) SETTINGS index_granularity = 8192;
+
+
+    CREATE MATERIALIZED VIEW IF NOT EXISTS dwh.mv_fact_game TO dwh._fact_game AS
+    SELECT
+        customer_id
+    , parseDateTimeBestEffortOrZero(nfg.activity_datetime) AS activity_datetime
+    , game_activity
+    , game_id
+    , game_type
+    , bet_amount
+    , win_amount
+    , bonus_amount
+        FROM
+            stage.null_fact_game AS nfg
+        GROUP BY customer_id, nfg.activity_datetime, game_activity, game_id, game_type, bet_amount, win_amount, bonus_amount;
+
+    -- marts
+    CREATE TABLE IF NOT EXISTS marts._daily_fact_game
+    (
+        customer_id   Int32,
+        activity_date Date,
+        game_id       Int32,
+        game_type LowCardinality(String),
+        bet_amount    Decimal(18, 4),
+        win_amount    Decimal(18, 4),
+        bonus_amount  Decimal(18, 4)
+    ) ENGINE = SummingMergeTree() PARTITION BY toYYYYMM(activity_date) ORDER BY (customer_id, activity_date, game_id, game_type) SETTINGS index_granularity = 8192;
+
+
+    CREATE MATERIALIZED VIEW IF NOT EXISTS marts.mv_daily_fact_game TO marts._daily_fact_game AS
+    SELECT
+        customer_id
+    , toDate(activity_datetime) AS activity_date
+    , game_id
+    , game_type
+    , sum(bet_amount)           as bet_amount
+    , sum(win_amount)           as win_amount
+    , sum(bonus_amount)         as bonus_amount
+        FROM
+            dwh._fact_game AS fg
+        GROUP BY customer_id, activity_date, game_id, game_type;
+
+
+    CREATE VIEW IF NOT EXISTS marts.daily_fact_game AS
+    SELECT
+        customer_id
+    , activity_date
+    , game_id
+    , game_type
+    , sum(bet_amount)   as bet_amount
+    , sum(win_amount)   as win_amount
+    , sum(bonus_amount) as bonus_amount
+        FROM
+            marts._daily_fact_game
+        GROUP BY customer_id, activity_date, game_id, game_type;   
+
+
+EOSQL
